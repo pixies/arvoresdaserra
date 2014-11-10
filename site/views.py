cb0782252a9cd4ab9a1ff374f886a190b98c514b
p1xies <cleyton@tagi.com.br>
2014-11-10 16:39:43 -0300

    Novos materiais

--- site/views.py
+++ site/views.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	icone = url_for('static', filename='images/arvore.png')
+	return render_template('index.html',icone=icone)
